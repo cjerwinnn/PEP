6db99b81802a6ab5a72ddab586bfecd7be37856f
jerwin <bfabbjerwin@gmail.com>
2025-06-26 21:06:12 +0800

    test

--- config/settings.php
+++ config/settings.php
@@ -12,18 +12,4 @@ function GetASF($conn) {
         return ['asf_percent' => 0.00];
     }
 }
-<<<<<<< HEAD
-=======
-
-function GetVAT($conn) {
-    $query = "SELECT vat_percent FROM ref_vat_percentage WHERE default_vat = 1";
-    $result = $conn->query($query);
-
-    if ($result) {
-        return $result->fetch_assoc();
-    } else {
-        return ['vat_percent' => 0.00];
-    }
-}
->>>>>>> 06b95ea615e0c6633d114500752a4081b56d7ba6
 ?>
